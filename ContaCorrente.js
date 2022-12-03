0dcd36a4e2845ea48c4099bbe96ace935afff8fd
Vitor Pacheco <pachecovitor090@gmail.com>
2022-12-03 19:01:42 -0300

    Adicionando assersores SET e GET
    Adicionado os assessores no môdulo ContaCorrente,
    
    Foi usado o SET CLIENTE para só atribuir se o novo valor for uma instancia de Cliente, ou seja, se for uma referencia cliente.
    
    E adicionado GET
    - GET SALDO E GET CLIENTE
    para consultar cliente e saldo

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,27 +1,46 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
   agencia;
-  cliente;
+  _cliente;
+
+  //Só atribui se o NOVO VALOR for uma instancia de CLiente
+  set cliente (novoValor) {
+    if (novoValor instanceof Cliente){
+      this._cliente = novoValor;
+    }
+  };
+
+  //para pegar o valor do cliente
+  get cliente () {
+    return this._cliente;
+  }
 
   _saldo = 0;
 
+  //para pegar o valor do saldo
+  get saldo () {
+    return this._saldo
+  }
+
   sacar(valor) {
-    //Verifica se o saldo da conta corrente é igual ou maior do que o valor de saque desejavel
+  //Verifica se o saldo da conta corrente é igual ou maior do que o valor de saque desejavel
     if (this._saldo >= valor) {
       this._saldo -= valor;
       return valor;
     }
-  }
+  };
 
   //Deposita o valor desejado caso o valor de deposito seja maior do que zero
   depositar(valor) {
     if (valor > 0) {
       this._saldo += valor;
     }
-  }
+  };
 
   //Realiza a transferencia do saldo de uma conta para a cnta (CONTA DE OUTRO USUARIO)
   transferir(valor, conta) {
     const valorSacado = this.sacar(valor);
-    conta.depositar(valor)
+    conta.depositar(valorSacado);
   }
-}
+};
--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import {ContaCorrente} from './ContaCorrente.js'
 
 const cliente1 = new Cliente();
 cliente1.nome = "Vitor";
-cliente1.cpf = 7321586219;
+cliente1.cpf = 111116219;
+cliente1.email = "pachecovitor090@gmail.com";
 
 const cliente2 = new Cliente();
 cliente2.nome = "Laura";
-cliente2.cpf = 6821570515;
+cliente2.cpf = 222210512;
+cliente2.email = "laura123@gmail.com";
 
 const contaCorrenteVitor = new ContaCorrente();
 contaCorrenteVitor._saldo = 0;
@@ -19,9 +21,12 @@ const contaCorrenteLaura = new ContaCorrente();
 contaCorrenteLaura.agencia = 1002;
 contaCorrenteLaura.cliente = cliente2;
 
+let valor = 200;
 
-contaCorrenteVitor.transferir(250, contaCorrenteLaura);
+contaCorrenteVitor.transferir(valor, contaCorrenteLaura);
 
-console.log(contaCorrenteVitor);
-console.log(contaCorrenteLaura);
+console.log(contaCorrenteVitor.cliente);
+console.log(contaCorrenteLaura.saldo);
+
+console.log(cliente1.saldo);
 
